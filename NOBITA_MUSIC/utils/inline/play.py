3c3199a2f1ff895e306c93a7e69333976474ac5e
Shaurya-0829 <shauryachaudhary2906@gmail.com>
2025-07-06 01:18:44 +0530

    𝚴 𝐎 𝐁 𝚰 𝐓 𝚲

--- NOBITA_MUSIC/utils/inline/play.py
+++ NOBITA_MUSIC/utils/inline/play.py
@@ -40,25 +40,25 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "▰▱▱▱▱▱▱▱▱▱"
+        bar = "◉—————————"
     elif 10 < umm < 20:
-        bar = "▰▰▱▱▱▱▱▱▱▱"
+        bar = "—◉————————"
     elif 20 <= umm < 30:
-        bar = "▰▰▰▱▱▱▱▱▱▱"
+        bar = "——𝚴———————"
     elif 30 <= umm < 40:
-        bar = "▰▰▰▰▱▱▱▱▱▱"
+        bar = "———𝐎——————"
     elif 40 <= umm < 50:
-        bar = "▰▰▰▰▰▱▱▱▱▱"
+        bar = "————𝐁—————"
     elif 50 <= umm < 60:
-        bar = "▰▰▰▰▰▰▱▱▱▱"
+        bar = "—————𝚰————"
     elif 60 <= umm < 70:
-        bar = "▰▰▰▰▰▰▰▱▱▱"
+        bar = "——————𝐓———"
     elif 70 <= umm < 80:
-        bar = "▰▰▰▰▰▰▰▰▱▱"
+        bar = "———————𝚲——"
     elif 80 <= umm < 95:
-        bar = "▰▰▰▰▰▰▰▰▰▱" 
+        bar = "————————◉—"
     else:
-        bar = "▰▰▰▰▰▰▰▰▰▰"
+        bar = "—————————◉"
 
     buttons = [
         [
